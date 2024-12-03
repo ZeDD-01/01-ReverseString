ef58739d65458798d9f13d2f6a40dd3ead4e44bd
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2024-12-03 22:56:30 +0100

    removed the check for 30 characters and the space + simplified the palindrome check

--- 01-ReverseString/Program.cs
+++ 01-ReverseString/Program.cs
@@ -11,37 +11,15 @@ class Program
         //I start at the end of the string "array", so I can assing the last letter from the string to the first position of the new string variable"
         for (int x = input.Length-1; x >= 0; x--)
         {
-            //here I check if the string is longer than 30 character or if it contains a space
-            if (input.Length > 30)
-            {
-                Console.Write("Your string is too long");
-                return;
-            }
-
-            if (input[x] == ' ')
-            {
-                Console.WriteLine("Your string contains a space");
-                return;
-            }
             
             reverseString += input[x];
         }
         
         Console.WriteLine("Your string: " +input+ " in reverse is: " + reverseString);
 
-        Boolean palindrome = false;
-        
-        for (int x = 0; x < input.Length; x++)
+        if (reverseString == input)
         {
-            if (input[x] == reverseString[x])
-            {
-                palindrome = true;
-            }
-        }
-
-        if (palindrome == true)
-        {
-            Console.WriteLine("Your string is also a palindrome");
+            Console.WriteLine("Your word is also a palindrome");
         }
     }
 }
